45836ae66c1700ef559e79ead705fc9b05b66b2a
ickejohannes <ickejohannes@gmail.com>
2022-05-12 14:07:07 +0200

    Do third iteration

--- js/index.js
+++ js/index.js
@@ -6,15 +6,46 @@ background.src = '../images/road.png';
 const car = new Image();
 car.src = "../images/car.png"
 
+const carInitialPosX = 120;
+const carInitialPosY = 330;
+let carPosX = carInitialPosX;
+let carPosY = carInitialPosY;
 
 
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
-    startGame();
+    updateCanvas();
+  };
+};
+
+
+function updateCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  ctx.drawImage(background, 0, 0);
+  ctx.drawImage(car, carPosX, carPosY, 37, 75);
+
+  requestAnimationFrame(updateCanvas)
+}
+
+
+document.addEventListener("keydown", event => {
+  console.log(event);
+  if (event.key == "ArrowLeft") {
+    moveCarToTheLeft();
   };
 
-  function startGame() {
-    ctx.drawImage(background, 0, 0);
-    ctx.drawImage(car, 120, 330, 37, 75);
-  }
-};
+  if (event.key == "ArrowRight") {
+    moveCarToTheRight();
+  };
+});
+
+
+function moveCarToTheLeft() {
+  if (carPosX <= 10) return;
+  carPosX -= 10;
+}
+
+function moveCarToTheRight() {
+  if (carPosX >= 235) return;
+  carPosX += 10;
+}
