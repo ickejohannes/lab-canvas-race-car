1164ef6a4d365db25f6a9a2e65a4960cb552e97f
ickejohannes <ickejohannes@gmail.com>
2022-05-12 12:33:17 +0200

    Do first iteration

--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+const img = new Image();
+img.src = '../images/road.png';
+
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    ctx.drawImage(img, 0, 0);
+  }
 };
